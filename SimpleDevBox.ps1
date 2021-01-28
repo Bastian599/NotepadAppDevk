10192e686f2debfe16ac04a2251570d561c684b0
Bastian599 <33347035+Bastian599@users.noreply.github.com>
2021-01-28 12:34:05 +0100

    Update SimpleDevBox.ps1

--- SimpleDevBox.ps1
+++ SimpleDevBox.ps1
@@ -1,7 +1,18 @@
-# Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Install Software
-choco install visualstudiocode -y
-choco install git -y 
-choco install nodejs-lts  -y
+#Assign Packages to Install
+$Packages = 'googlechrome',`
+            'visualstudiocode',`
+            'git',`
+            'visualstudio2017community',`
+            'visualstudio2017-workload-azure',`
+            'visualstudio2017-workload-netweb'
+
+#Install Packages
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+#Reboot
+Restart-Computer
