58c8fe880ec4bef534a940c482d2ffbb0fb12f04
Bastian599 <33347035+Bastian599@users.noreply.github.com>
2021-02-01 12:20:21 +0100

    Update vpn.ps1

--- vpn.ps1
+++ vpn.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 cd $env:USERPROFILE\Downloads
-wget "https://software.sonicwall.com/NetExtender/NetExtender-10.2.302.MSI"
+Invoke-WebRequest -Uri "https://software.sonicwall.com/NetExtender/NetExtender-10.2.302.MSI" -OutFile "$env:USERPROFILE\Downloads\install.msi"
 Start-Process msiexec.exe -Wait -ArgumentList '/I $env:USERPROFILE\Downloads\install.msi /quiet'
 return
